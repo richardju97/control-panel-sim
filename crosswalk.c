c418bb0345725febfa7d373567ccf5fbb8f98d14
rju <rju@purdue.edu>
2017-03-12 07:53:56 +0000

    Initial commit of crosswalk.c

---
+++ crosswalk.c
@@ -0,0 +1,19 @@
+//cross-walk.c
+
+#include <softPwm.h>
+#include <wiringPi.h>
+
+//first side of the crosswalk
+//note that this side uses an RGB LED
+#define RED1 0
+#define GREEN1 2
+#define BLUE1 3
+
+//second side of the crosswalk
+//note that this side uses a White LED and a Red LED
+#define RED2 4
+#define WHITE 5
+
+int main() {
+	
+}
