26ee9fce118e5d5aeac2d78c9e2afed28b8e4e67
rju <rju@purdue.edu>
2017-03-12 08:02:22 +0000

    Added majority of constants needed.

--- crosswalk.c
+++ crosswalk.c
@@ -14,6 +14,28 @@
 #define RED2 4
 #define WHITE 5
 
+//universal constants across the entire system
+#define ON 50
+#define MAXBRIGHTNESS 100
+#define FLASHDELAY 1000
+#define FLASH 5000
+#define WALK 2500
+#define CYCLE 10000
+
 int main() {
 	
+	if (wiringPiSetup() == 01) {
+		return 1; //return error status (initialization failed)
+	}	
+
+	int r1 = 0;
+	int g = 0;
+	int b = 0;
+	
+	int r2 = 0;
+	int w = 0;
+
+	
+
+	return 0;
 }
