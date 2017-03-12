368b41d64018c80e10fb33fb8f8dbe5348048430
rju <rju@purdue.edu>
2017-03-12 08:06:53 +0000

    Initialized all buttons and GPIO Pins for LEDs

--- crosswalk.c
+++ crosswalk.c
@@ -6,17 +6,19 @@
 //first side of the crosswalk
 //note that this side uses an RGB LED
 #define RED1 0
-#define GREEN1 2
-#define BLUE1 3
+#define GREEN 2
+#define BLUE 3
+#define BUTTON1 26
 
 //second side of the crosswalk
 //note that this side uses a White LED and a Red LED
 #define RED2 4
 #define WHITE 5
+#define BUTTON2 27
 
 //universal constants across the entire system
 #define ON 50
-#define MAXBRIGHTNESS 100
+#define MAX 100
 #define FLASHDELAY 1000
 #define FLASH 5000
 #define WALK 2500
@@ -35,7 +37,20 @@ int main() {
 	int r2 = 0;
 	int w = 0;
 
-	
+	pinMode(BUTTON1, INPUT);
+	pinMode(BUTTON2, INPUT);
+
+	while (1) {
+		
+		softPwmCreate(RED1, r1, MAX);
+		softPwmCreate(GREEN, g, MAX);
+		softPwmCreate(BLUE, b, MAX);
+		softPwmCreate(RED2, r2, MAX);
+		softPwmCreate(WHITE, w, MAX);
+
+		
+
+	}	
 
 	return 0;
 }
